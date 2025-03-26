497b8fe4205e63b7c512dd52ada020c6eca21826
hoodiedan <obodedaniel3@gmail.com>
2025-03-26 03:48:05 +0100

    feat: refactor SortPopup component to use Form and Select for improved validation and user experience; update sorting logic and options

--- src/components/SortPopup.vue
+++ src/components/SortPopup.vue
@@ -8,41 +8,69 @@
         </div>
 
         <p class="mt-4">Sorting Direction</p>
-        <select
-            v-model="selectedSortOrder"
-            class="w-full rounded-sm bg-anti-flash-white p-3"
-        >
-            <option value="asc">Lowest - Highest Price</option>
-            <option value="desc">Highest - Lowest Price</option>
-        </select>
+        <Form v-slot="$form" :initialValues :resolver @submit="onFormSubmit" class="flex flex-col gap-4 w-full sm:w-56">
+            <Select name="order" v-model="selectedSortOrder" :options="options" optionLabel="name" class="w-full rounded-sm bg-anti-flash-white py-2 border-0" fluid>
+            </Select>
 
-        <div class="mt-3 flex gap-3">
-            <Button class="w-1/3 bg-anti-flash-white text-black cursor-pointer border-0 py-3" @click="reset">Reset</Button>
-            <Button class="w-2/3 bg-black text-white cursor-pointer border-0 py-3" @click="updateSort">Apply</Button>
-        </div>
+            <Message v-if="$form.order?.invalid" severity="error" size="small" variant="simple">{{
+                $form.order.error?.message
+            }}</Message>
+
+            <div class="mt-3 flex gap-3">
+                <Button class="w-1/3 bg-anti-flash-white text-black cursor-pointer border-0 py-3" @click="reset"
+                    >Reset</Button
+                >
+                <Button
+                    class="w-2/3 bg-black text-white cursor-pointer border-0 py-3"
+                    type="submit"
+                    severity="secondary"
+                    label="Apply"
+                />
+            </div>
+        </Form>
     </div>
 </template>
 
 <script setup>
-import { ref, defineEmits, watchEffect } from "vue";
+import { ref, defineEmits, reactive } from "vue";
 import { useProductStore } from "../stores/product";
 import { Check } from "lucide-vue-next";
 
 const emit = defineEmits(["closePopup"]);
 
 const { selectSortOrder, resetSort, sortOrder } = useProductStore();
-const selectedSortOrder = ref(sortOrder.value); // Initialize with current sort
+const selectedSortOrder = ref(sortOrder);
+const options = ref([
+    { name: 'Lowest - Highest Price', code: 'asc' },
+    { name: 'Highest - Lowest Price', code: 'desc' },
+]);
 
-// Keep selectedSortOrder in sync with store value
-watchEffect(() => {
-    selectedSortOrder.value = sortOrder.value;
+const initialValues = reactive({
+    order: selectedSortOrder,
 });
 
+const resolver = ({ values }) => {
+    const errors = {};
+
+    if (values.order === "def") {
+        errors.order = [{ message: "Please enter a valid order" }];
+    }
+
+    return {
+        values,
+        errors,
+    };
+};
+
+const onFormSubmit = ({ valid, values }) => {
+    if (valid) {
+        updateSort(values.order.code);
+    }
+};
+
 // Function to apply sorting
-const updateSort = () => {
-    console.log(selectedSortOrder);
-    
-    selectSortOrder(selectedSortOrder.value);
+const updateSort = (val) => {
+    selectSortOrder(val);
     emit("closePopup");
 };
 
